91db0edd2e23c615fc3826b711d8eb3191fd136f
yemisrach <meserlengereh@gmail.com>
2017-11-08 17:06:35 -0500

    Initial commit

---
+++ src/main/java/com/example/springbooot_10/Springbooot10Application.java
@@ -0,0 +1,12 @@
+package com.example.springbooot_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbooot10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbooot10Application.class, args);
+	}
+}
